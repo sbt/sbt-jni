21e05d7a638899c5f31283e95634b446fa6a2f5a
Jakob Odersky <jakob@odersky.com>
2016-10-17 00:54:51 -0700

    Upgrade scala version

--- plugin/src/sbt-test/sbt-jni/simple/project/ScriptedHelper.scala
+++ plugin/src/sbt-test/sbt-jni/simple/project/ScriptedHelper.scala
@@ -8,7 +8,7 @@ object ScriptedHelper extends AutoPlugin {
 
   override def projectSettings = Seq(
     scalacOptions ++= Seq("-feature", "-deprecation"),
-    crossScalaVersions := Seq("2.11.8", "2.10.6", "2.12.0-RC1"),
+    crossScalaVersions := Seq("2.11.8", "2.10.6", "2.12.0-RC2"),
     scalaVersion := crossScalaVersions.value.head
   )
 
