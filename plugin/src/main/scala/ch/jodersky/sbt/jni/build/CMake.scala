a063f51d8a7392d5e1439d32edd161e233dd0c36
Martin Duhem <martin.duhem@gmail.com>
2021-04-19 09:29:23 +0200

    Bump to newer Visual Studio

--- plugin/src/main/scala/ch/jodersky/sbt/jni/build/CMake.scala
+++ plugin/src/main/scala/ch/jodersky/sbt/jni/build/CMake.scala
@@ -23,7 +23,7 @@ object CMake extends BuildTool with ConfigureMakeInstall {
     override def buildDirectory = buildDir 
 
     private val cmakeCmd = if(OsAndArch.IsWindows && (System.getProperty("os.arch").toUpperCase == "AMD64")) {
-         """cmake -G"Visual Studio 15 2017 Win64""""
+         """cmake -G"Visual Studio 16 2019 Win64""""
       } else {
         "cmake"
       }
