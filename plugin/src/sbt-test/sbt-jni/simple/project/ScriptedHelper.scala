a6ac8e30621aceb77c2da6de720f43bce11ee44b
Grigory Pomadchin <gr.pomadchin@gmail.com>
2021-07-22 17:08:38 -0400

    Temporary remove Scala 3 from tests

--- plugin/src/sbt-test/sbt-jni/simple/project/ScriptedHelper.scala
+++ plugin/src/sbt-test/sbt-jni/simple/project/ScriptedHelper.scala
@@ -8,7 +8,7 @@ object ScriptedHelper extends AutoPlugin {
 
   override def projectSettings = Seq(
     scalacOptions ++= Seq("-feature", "-deprecation"),
-    crossScalaVersions := Seq("3.0.0", "2.13.6", "2.12.14"),
+    crossScalaVersions := Seq("2.13.6", "2.12.14"),
     scalaVersion := crossScalaVersions.value.head
   )
 
