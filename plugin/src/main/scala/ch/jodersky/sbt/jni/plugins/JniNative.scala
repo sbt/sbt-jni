e0a827015695af7472f7200b84dfe87b8f0c1e02
Ondra Pelech <ondra.pelech@gmail.com>
2021-07-26 14:19:39 +0200

    Reuse BuildTool.buildTools

--- plugin/src/main/scala/ch/jodersky/sbt/jni/plugins/JniNative.scala
+++ plugin/src/main/scala/ch/jodersky/sbt/jni/plugins/JniNative.scala
@@ -62,7 +62,7 @@ object JniNative extends AutoPlugin {
     nativeCompile / sourceDirectory := sourceDirectory.value / "native",
     nativeCompile / target := target.value / "native" / nativePlatform.value,
     nativeBuildTool := {
-      val tools = Seq(CMake, Cargo.release)
+      val tools = BuildTool.buildTools.values.toList
 
       val src = (nativeCompile / sourceDirectory).value
 
