08f2a7eaec581df5a9720fd2bf9931cf19ce1ef7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-15 02:24:35 +0100

    Update scala-compiler, scala-library, ... to 2.13.8 (#94)

--- plugin/src/sbt-test/sbt-jni/simple-syntax/project/ScriptedHelper.scala
+++ plugin/src/sbt-test/sbt-jni/simple-syntax/project/ScriptedHelper.scala
@@ -8,7 +8,7 @@ object ScriptedHelper extends AutoPlugin {
 
   override def projectSettings = Seq(
     scalacOptions ++= Seq("-feature", "-deprecation"),
-    crossScalaVersions := Seq("3.1.0", "2.13.7", "2.12.15"),
+    crossScalaVersions := Seq("3.1.0", "2.13.8", "2.12.15"),
     scalaVersion := crossScalaVersions.value.head
   )
 
