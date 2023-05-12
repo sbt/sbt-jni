f1592be258256887953b20b9cd714268c66f0d52
Mihnea Stoian <mihn.stn@gmail.com>
2023-05-12 13:27:50 +0200

    Add support for Meson build tool

---
+++ plugin/src/main/scala/com/github/sbt/jni/build/Meson.scala
@@ -0,0 +1,76 @@
+package com.github.sbt.jni
+package build
+
+import sbt._
+import sys.process._
+
+class Meson(protected val configuration: Seq[String]) extends BuildTool with ConfigureMakeInstall {
+
+  override val name = Meson.name
+
+  override def detect(baseDirectory: File) = baseDirectory.list().contains("meson.build")
+
+  override protected def templateMappings = Seq(
+    "/com/github/sbt/jni/templates/meson.build" -> "meson.build",
+    "/com/github/sbt/jni/templates/meson.options" -> "meson.options"
+  )
+
+  override def getInstance(baseDir: File, buildDir: File, logger: Logger) = new Instance {
+
+    override def log = logger
+    override def baseDirectory = baseDir
+    override def buildDirectory = buildDir
+
+    def mesonProcess(args: String*): ProcessBuilder = Process("meson" +: args, buildDirectory)
+
+    lazy val mesonVersion = mesonProcess("--version").lineStream.head.replace('.', '_')
+
+    lazy val mesonBuildDir = buildDirectory / mesonVersion
+
+    override def configure(target: File) = {
+      mesonProcess(
+        Seq("setup", "--prefix", target.getAbsolutePath) ++ configuration ++ Seq(
+          mesonVersion.toString,
+          baseDirectory.getAbsolutePath
+        ): _*
+      )
+    }
+
+    override def clean(): Unit = mesonProcess(
+      "compile",
+      "-C",
+      mesonBuildDir.getAbsolutePath,
+      "--clean"
+    ).run(log)
+
+    override def make(): ProcessBuilder = mesonProcess(
+      "compile",
+      "-C",
+      mesonBuildDir.getAbsolutePath,
+      "--jobs",
+      parallelJobs.toString
+    )
+
+    override def install(): ProcessBuilder = mesonProcess(
+      "install",
+      "-C",
+      mesonBuildDir.getAbsolutePath
+    )
+  }
+
+}
+
+object Meson {
+  val name = "Meson"
+  val DEFAULT_CONFIGURATION = Seq("--buildtype=release", "-Dsbt=true")
+
+  /**
+   * Meson setup configuration arguments.
+   */
+  def make(configuration: Seq[String] = DEFAULT_CONFIGURATION): BuildTool = new Meson(configuration)
+
+  /**
+   * Meson build tool, with the release build type.
+   */
+  lazy val release: BuildTool = make()
+}
---
+++ plugin/src/sbt-test/sbt-jni/simple-meson/build.sbt
@@ -0,0 +1,12 @@
+ivyLoggingLevel := UpdateLogging.Quiet
+
+lazy val root = (project in file(".")).aggregate(core, native)
+
+lazy val core = project
+  .settings(libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.15" % Test)
+  .settings(javah / target := (native / nativeCompile / sourceDirectory).value / "include")
+  .dependsOn(native % Runtime)
+
+lazy val native = project
+  .settings(nativeCompile / sourceDirectory := sourceDirectory.value)
+  .enablePlugins(JniNative)
