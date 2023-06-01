48479f26eeb8201151db3c1f5d05656d50469b42
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-06-01 03:11:08 +0200

    Update scala3-compiler, scala3-library to 3.3.0 (#150)

--- plugin/src/sbt-test/sbt-jni/simple-syntax/project/ScriptedHelper.scala
+++ plugin/src/sbt-test/sbt-jni/simple-syntax/project/ScriptedHelper.scala
@@ -8,7 +8,7 @@ object ScriptedHelper extends AutoPlugin {
 
   override def projectSettings = Seq(
     scalacOptions ++= Seq("-feature", "-deprecation"),
-    crossScalaVersions := Seq("3.2.2", "2.13.10", "2.12.17"),
+    crossScalaVersions := Seq("3.3.0", "2.13.10", "2.12.17"),
     scalaVersion := crossScalaVersions.value.head
   )
 
