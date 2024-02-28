db7c6ccfb522d40fdf5d1b555b5b85cb6717ae6e
Scala Steward <scala_steward@virtuslab.com>
2024-02-28 04:15:30 +0000

    Update scala-compiler, scala-library, ... to 2.12.19

--- plugin/src/sbt-test/sbt-jni/simple-meson/project/ScriptedHelper.scala
+++ plugin/src/sbt-test/sbt-jni/simple-meson/project/ScriptedHelper.scala
@@ -8,7 +8,7 @@ object ScriptedHelper extends AutoPlugin {
 
   override def projectSettings = Seq(
     scalacOptions ++= Seq("-feature", "-deprecation"),
-    crossScalaVersions := Seq("2.13.12", "2.12.18"),
+    crossScalaVersions := Seq("2.13.12", "2.12.19"),
     scalaVersion := crossScalaVersions.value.head
   )
 
